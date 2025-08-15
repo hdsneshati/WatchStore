02aa1e345d5290c9331f2a812996c608910d108c
hdsneshati <hadisneshati1384@gmail.com>
2025-08-16 01:45:47 +0330

    add send & get otp screen

--- watch_store/lib/screens/get_otp_screen.dart
+++ watch_store/lib/screens/get_otp_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:watch_store/component/extention.dart';
+import 'package:watch_store/core/widgets/primary_button.dart';
+import 'package:watch_store/res/dimens.dart';
+import 'package:watch_store/res/strings.dart';
+import 'package:watch_store/core/widgets/app_text_feild.dart';
 
 class GetOtpScreen extends StatelessWidget {
-  const GetOtpScreen({super.key});
+  GetOtpScreen({super.key});
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            (size.height * 0.2).verticalSpace,
+            Image.asset('assets/png/main_logo.png'),
+            AppDimens.large.verticalSpace,
+            Text(AppStrings.otpCodeSendFor
+                .replaceAll(AppStrings.replace, '09024365997')),
+            AppDimens.small.verticalSpace,
+            Text(AppStrings.wrongNumberEditNumber),
+            AppDimens.large.verticalSpace,
+            AppTextFeild(
+              lable: AppStrings.enterVerificationCode,
+              hint: AppStrings.hintVerificationCode,
+              //  icon:Image.asset('assets/svg/avatar.svg'),
+              //  inputType: TextInputType.phone,
+              prefixlabel: '2:30',
+              controller: controller,
+            ),
+            PrimaryButton(
+                label: AppStrings.next,
+                onPressed: () {
+                  // Handle button press
+                }),
+          ],
+        ),
+      )),
+    );
   }
 }
--- watch_store/lib/screens/send_otp_screen.dart
+++ watch_store/lib/screens/send_otp_screen.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:watch_store/component/extention.dart';
+import 'package:watch_store/core/widgets/primary_button.dart';
+import 'package:watch_store/res/dimens.dart';
+import 'package:watch_store/res/strings.dart';
+import 'package:watch_store/core/widgets/app_text_feild.dart';
 
 class SendOtpScreen extends StatelessWidget {
-  const SendOtpScreen({super.key});
-
+   SendOtpScreen({super.key});
+    TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           Image.asset('assets/png/mainLogo'),
-           
-        ],
+      body: SafeArea(child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          (size.height*0.2).verticalSpace,
+             Image.asset('assets/png/main_logo.png'),
+             AppDimens.large.verticalSpace,
+             AppTextFeild(
+              lable:AppStrings.enterYourNumber ,
+              hint: AppStrings.hintPhoneNumber,
+            //  icon:Image.asset('assets/svg/avatar.svg'),
+            //  inputType: TextInputType.phone,
+              controller: controller,
+               
+             ),
+             PrimaryButton(
+              label: AppStrings.next, 
+              onPressed: (){
+                // Handle button press
+              }),
+          ],
+        ),
       )),
     );
   }
