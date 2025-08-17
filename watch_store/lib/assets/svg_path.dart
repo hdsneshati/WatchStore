259b171afdfbef1116281a3977844b0fba20b19c
hdsneshati <hadisneshati1384@gmail.com>
2025-08-17 12:28:39 +0330

    add some style and svg path

---
+++ watch_store/lib/assets/svg_path.dart
@@ -0,0 +1,8 @@
+class SvgPath {
+  SvgPath._();
+
+ 
+  static const String avatar = "assets/svg/user_menu.svg";
+  static const String product = "assets/svg/cart.svg";
+  static const String home = "assets/svg/home.svg";
+}
