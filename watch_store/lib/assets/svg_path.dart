45e0aa8276d38261e5b4bc5318a11c320a5f3d64
hdsneshati <hadisneshati1384@gmail.com>
2025-08-18 15:14:05 +0330

    add svg

--- watch_store/lib/assets/svg_path.dart
+++ watch_store/lib/assets/svg_path.dart
@@ -5,4 +5,11 @@ class SvgPath {
   static const String avatar = "assets/svg/user_menu.svg";
   static const String product = "assets/svg/cart.svg";
   static const String home = "assets/svg/home.svg";
+  static const String clasic = "assets/svg/clasic.svg";
+  static const String smart = "assets/svg/smart.svg";
+  static const String digital = "assets/svg/digital.svg";
+  static const String desktop = "assets/svg/desktop.svg";
+  static const String verticalIcon = "assets/svg/back.svg";
+  static const String close = "assets/svg/close.svg";
+  static const String sort = "assets/svg/sort.svg";
 }
