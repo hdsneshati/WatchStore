704d60de541bcabb96f0ad3cadf35c7f235eaf9f
hdsneshati <hadisneshati1384@gmail.com>
2025-08-18 15:12:28 +0330

    add productappbar

---
+++ watch_store/lib/features/product/widgets/cart_badge.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:size_config/size_config.dart';
+import 'package:watch_store/assets/svg_path.dart';
+
+class CartBadge extends StatelessWidget {
+  const CartBadge({this.count = 0, super.key});
+  final int count; // Example count for the badge
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SvgPicture.asset(
+          SvgPath.product,
+          colorFilter:const ColorFilter.mode(
+            Colors.black,
+            BlendMode.srcIn,
+          ),
+        ),
+        Visibility(
+          visible: count > 0 ? true : false,
+          child: Positioned(
+            right: 0,
+            top: 0,
+            child: Container(
+              width: 17.w,
+              height: 17.h,
+              decoration:const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  '$count',
+                  style:const TextStyle(
+                    color: Colors.white,
+                    
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
