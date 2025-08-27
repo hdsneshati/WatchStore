7b9b2ec95bdf47d0581e7e613e1d510fa99bf243
hdsneshati <hadisneshati1384@gmail.com>
2025-08-27 13:14:43 +0330

    add user&phone&cart icon

--- watch_store/lib/assets/svg_path.dart
+++ watch_store/lib/assets/svg_path.dart
@@ -12,4 +12,11 @@ class SvgPath {
   static const String verticalIcon = "assets/svg/back.svg";
   static const String close = "assets/svg/close.svg";
   static const String sort = "assets/svg/sort.svg";
+  static const String laftArrow = "assets/svg/left_arrow.svg";
+  static const String delete = "assets/svg/delete.svg";
+  static const String minus = "assets/svg/minus.svg";
+  static const String plus = "assets/svg/plus.svg";
+  static const String user = "assets/svg/user.svg";
+  static const String phone = "assets/svg/phone.svg";
+  static const String cart = "assets/svg/cart.svg";
 }
