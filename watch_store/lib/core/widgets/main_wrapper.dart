1d166b8726beaa72082d1b0b64b45dbe6c64533b
hdsneshati <hadisneshati1384@gmail.com>
2025-08-27 13:16:00 +0330

    add const on mainwrapper

--- watch_store/lib/core/widgets/main_wrapper.dart
+++ watch_store/lib/core/widgets/main_wrapper.dart
@@ -1,8 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:watch_store/assets/svg_path.dart';
+import 'package:watch_store/core/widgets/cart_badge.dart';
 import 'package:watch_store/features/home/Screens/homescreen.dart';
 import 'package:watch_store/features/product/screens/productscreen.dart';
 import 'package:watch_store/features/profile/profilescreen.dart';
@@ -15,20 +14,17 @@ class MainWrapper extends StatefulWidget {
 }
 
 class _MainWrapperState extends State<MainWrapper> {
-
   int index = 0;
   final List<int> _routeHistory = [0];
   List<Widget> screens = [
     const HomeScreen(),
     const ProductScreen(),
     const ProfileScreen(),
-    
   ];
   List<String> appBarTitles = [
     "خانه",
     "سبد خرید",
     "پروفایل",
-   
   ];
 
   @override
@@ -39,27 +35,24 @@ class _MainWrapperState extends State<MainWrapper> {
     super.initState();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return PopScope(
-       canPop: false, // از بستن مستقیم جلوگیری می‌کنیم
+      canPop: false, // از بستن مستقیم جلوگیری می‌کنیم
       onPopInvokedWithResult: (context, result) {
-                 print(_routeHistory.length);
+        print(_routeHistory.length);
 
         if (_routeHistory.length >= 1) {
           print(_routeHistory.first);
           setState(() {
-            _routeHistory.removeLast(); 
-            print( _routeHistory.length);
-            index = _routeHistory.last; 
+            _routeHistory.removeLast();
+            print(_routeHistory.length);
+            index = _routeHistory.last;
           });
         } else {
           // اگه هیستوری خالی شد → اپ بسته بشه
-         
         }
-        },
+      },
       child: Scaffold(
         bottomNavigationBar: NavigationBar(
           elevation: 10,
@@ -69,55 +62,46 @@ class _MainWrapperState extends State<MainWrapper> {
           onDestinationSelected: (value) {
             setState(() {
               index = value;
-              _routeHistory.add(index); 
+              _routeHistory.add(index);
             });
-             
-            
-              },
+          },
           destinations: <Widget>[
-              NavigationDestination(
-              icon: 
-              SvgPicture.asset(
-                SvgPath.home,
-                colorFilter:const ColorFilter.mode(
-                    Colors.grey,
-                    BlendMode.srcIn),
-              ),
-             
-              selectedIcon:
-               SvgPicture.asset(
-                SvgPath.home,
-                colorFilter:const ColorFilter.mode(
-                    Colors.black, BlendMode.srcIn),
-              ),
-              label: 'خانه',
-              
-            ),
             NavigationDestination(
               icon: SvgPicture.asset(
-                SvgPath.product,
-                colorFilter:const ColorFilter.mode(
-                    Colors.grey,
-                    BlendMode.srcIn),
+                SvgPath.home,
+                colorFilter:
+                    const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
               ),
               selectedIcon: SvgPicture.asset(
-                SvgPath.product,
-                colorFilter:const ColorFilter.mode(
-                    Colors.black, BlendMode.srcIn),
+                SvgPath.home,
+                colorFilter:
+                    const ColorFilter.mode(Colors.black, BlendMode.srcIn),
+              ),
+              label: 'خانه',
+            ),
+            NavigationDestination(
+              icon: CartBadge(
+                count: 5,
+                colorfilter:
+                    const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+              ),
+              selectedIcon: CartBadge(
+                count: 5,
+                colorfilter:
+                    const ColorFilter.mode(Colors.black, BlendMode.srcIn),
               ),
               label: 'سبد خرید',
             ),
             NavigationDestination(
               icon: SvgPicture.asset(
                 SvgPath.avatar,
-                colorFilter:const ColorFilter.mode(
-                    Colors.grey,
-                    BlendMode.srcIn),
+                colorFilter:
+                    const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
               ),
               selectedIcon: SvgPicture.asset(
                 SvgPath.avatar,
-                colorFilter:const ColorFilter.mode(
-                    Colors.black, BlendMode.srcIn),
+                colorFilter:
+                    const ColorFilter.mode(Colors.black, BlendMode.srcIn),
               ),
               label: 'پروفایل',
             ),
@@ -127,6 +111,7 @@ class _MainWrapperState extends State<MainWrapper> {
       ),
     );
   }
+
   btmNavOnPress(int index) {
     setState(() {
       this.index = index;
@@ -134,6 +119,3 @@ class _MainWrapperState extends State<MainWrapper> {
     });
   }
 }
-
-
-
